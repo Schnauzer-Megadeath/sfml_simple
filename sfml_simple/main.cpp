257e89a64d9413fa82205b2280a8777fbf3f39ab
MOTHMAN-404 <shawnmcdermott17@gmail.com>
2022-05-25 17:01:48 -0400

    Initial build

---
+++ sfml_simple/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	std::cin.get();
+}
